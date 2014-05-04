5925807183b712763aaedade6ec62c54a5cc051f
plugintpv <plugintpv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-04 14:21:08 +0000

    tagging version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-servired-integration-light/tags/1.4@907762 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-servired-integration-light.php
+++ woocommerce-servired-integration-light.php
@@ -25,7 +25,7 @@
  * Description: Pago por tarjeta servired para Woocommerce. Versión Light.
  * Author: PluginTPV
  * Author URI: http://www.plugintpv.com
- * Version: 1.3.1
+ * Version: 1.4
  */
 
 if ( !defined( 'WOOCOMMERCE_SERVIRED_LIGHT_URL' ) ) {
